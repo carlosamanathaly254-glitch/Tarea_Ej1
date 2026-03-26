fa38613d7c643700dfb8be5d58e7e4f6bcab6e1c
NathalyC <carlosamanathaly254@gmail.com>
2026-03-26 17:24:25 -0500

    Avance ejercicio 1

---
+++ src/Mascota.java
@@ -0,0 +1,27 @@
+/*
+Ejercicio 1 — Clase Mascota
+Autor: Nathaly Carlosama
+
+Enunciado: 
+    Crea una clase llamada Mascota que represente una mascota sencilla.
+*/
+
+public class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    public Mascota(String nombre, String especie, int edad){
+        this.nombre= nombre;
+        this.especie= especie;
+        this.edad= edad;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Especie: "+especie);
+        System.out.println("Edad: "+edad);
+    }
+
+    
+}
