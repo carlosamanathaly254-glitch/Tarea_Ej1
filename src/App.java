801e0e40cb158508e69d5123032a86b2ab38d4b7
NathalyC <carlosamanathaly254@gmail.com>
2026-03-26 17:30:42 -0500

    Crear mascota

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Mascota m1= new Mascota("Pedro", "Perico", 9);
+        Mascota m2= new Mascota("Diego", "Gato", 5);
+
+        m1.mostrarInfo();
+        m2.mostrarInfo();
     }
 }
